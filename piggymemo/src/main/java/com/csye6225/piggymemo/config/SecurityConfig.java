445aeb0cf5799cff86f8229f7e7d6fef257d64df
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-17 17:53:54 -0400

    Added initial security config.

--- piggymemo/src/main/java/com/csye6225/piggymemo/config/SecurityConfig.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/config/SecurityConfig.java
@@ -1,5 +1,55 @@
 package com.csye6225.piggymemo.config;
 
-public class SecurityConfig {
+import java.util.List;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(
+                auth -> auth
+                    .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/users/*").permitAll()
+                    .requestMatchers("/api/auth/**").permitAll()
+            )
+            .formLogin(f -> f.disable())
+            .httpBasic(b -> b.disable())
+            .cors(cors -> {})
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(
+            "http://localhost:3000",
+            "https://piggymemo.vercel.app"
+        ));
+        config.setAllowedMethods(List.of(
+            "GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"
+        ));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
