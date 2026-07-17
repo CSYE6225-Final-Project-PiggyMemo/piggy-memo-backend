bc2b9cd97577c35592c501c8cfde649c01c856ad
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-17 17:54:05 -0400

    Added a comment

--- piggymemo/src/main/java/com/csye6225/piggymemo/config/SecurityConfig.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            //For easier debugging. Enable when deployed.
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(
                 auth -> auth
