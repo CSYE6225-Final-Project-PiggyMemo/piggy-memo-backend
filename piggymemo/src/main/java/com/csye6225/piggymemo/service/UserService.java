e21cc34524080d7d4279cd85a74fec52ef06a2a2
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-17 21:48:44 -0400

    Added user creation service

--- piggymemo/src/main/java/com/csye6225/piggymemo/service/UserService.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/service/UserService.java
@@ -2,6 +2,10 @@ package com.csye6225.piggymemo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.csye6225.piggymemo.dto.CreateUserRequest;
+import com.csye6225.piggymemo.dto.UserResponse;
+import com.csye6225.piggymemo.entity.User;
+import com.csye6225.piggymemo.exception.UsernameAlreadyExistsException;
 import com.csye6225.piggymemo.repository.UserRepository;
 
 @Service
@@ -11,5 +15,28 @@ public class UserService {
     public UserService(
         UserRepository userRepository){
             this.userRepository = userRepository;
+    }
+
+    public UserResponse createUser(CreateUserRequest req) {
+        if(userRepository.existsByUsername(req.getUsername()))
+            throw new UsernameAlreadyExistsException("Username " + req.getUsername() + " already exists!");
+
+        User user = new User();
+        user.setUsername(req.getUsername());
+        user.setPassword(req.getPassword());
+
+        User save = userRepository.save(user);
+
+        return toResponse(save);
+    }
+
+    public UserResponse toResponse(User user) {
+        UserResponse response = new UserResponse();
+
+        response.setId(user.getId());
+        response.setUsername(user.getUsername());
+        response.setCreatedAt(user.getCreatedAt());
+
+        return response;
     }
 }
