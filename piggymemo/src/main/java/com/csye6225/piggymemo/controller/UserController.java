8e4057718d20321ebdbfe5a54c2102d517e95180
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-17 22:06:04 -0400

    Added API controller for user creation

--- piggymemo/src/main/java/com/csye6225/piggymemo/controller/UserController.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/controller/UserController.java
@@ -1,5 +1,30 @@
 package com.csye6225.piggymemo.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.csye6225.piggymemo.dto.CreateUserRequest;
+import com.csye6225.piggymemo.dto.UserResponse;
+import com.csye6225.piggymemo.service.UserService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public UserResponse create(@Valid @RequestBody CreateUserRequest req) {
+        return userService.createUser(req);
+    }
 }
