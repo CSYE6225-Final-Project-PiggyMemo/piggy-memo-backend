06c462cbc88f290f255fb8943a1380b59a8e8be5
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-17 21:30:34 -0400

    Added existed username exception and corresponding handler

--- piggymemo/src/main/java/com/csye6225/piggymemo/exception/GlobalExceptionHandler.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,17 @@
 package com.csye6225.piggymemo.exception;
 
-public class GlobalExceptionHandler {
+import java.util.Map;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Map<String, String> handleDuplicatedUsername(UsernameAlreadyExistsException e) {
+        return Map.of("message", e.getMessage());
+    }
 }
---
+++ piggymemo/src/main/java/com/csye6225/piggymemo/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.csye6225.piggymemo.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
