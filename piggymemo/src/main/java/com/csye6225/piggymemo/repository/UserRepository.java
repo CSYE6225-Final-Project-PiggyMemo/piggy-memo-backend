35394b443135cbf1db8c3fdc9a85bb74bb7d7e44
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-23 08:33:15 -0400

    Added jwt token issueing and cookie encapsulation

--- piggymemo/src/main/java/com/csye6225/piggymemo/repository/UserRepository.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.csye6225.piggymemo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.csye6225.piggymemo.entity.User;
-import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public boolean existsByUsername(String username);
-    public User findByUsername(String username);
+    public Optional<User> findByUsername(String username);
 }
--- piggymemo/src/main/java/com/csye6225/piggymemo/service/AuthService.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/service/AuthService.java
@@ -1,7 +1,16 @@
 package com.csye6225.piggymemo.service;
 
+import java.time.Duration;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.csye6225.piggymemo.dto.LoginRequest;
+import com.csye6225.piggymemo.entity.User;
 import com.csye6225.piggymemo.repository.UserRepository;
 
 public class AuthService {
@@ -13,5 +22,35 @@ public class AuthService {
         this.jwtService = jwtService;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public ResponseEntity<Map<String, String>> login(LoginRequest req) {
+        String username = req.getUsername();
+        String password = req.getPassword();
+        
+        String token = validateAndGetToken(username, password);
+        //Generate cookie and encapsulate token
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+            .httpOnly(true)
+            .path("/")
+            .sameSite("None")
+            .secure(true)
+            .maxAge(Duration.ofHours(1))
+            .build();
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
+            .body(Map.of("message", "Login success"));
+    }
+
+    private String validateAndGetToken(String username, String password) {
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+            new BadCredentialsException("Wrong username or password, please check your information."));
+        
+        if(!(passwordEncoder.matches(password, user.getPassword()))) {
+            throw new BadCredentialsException("Wrong username or password, please check your information.");
+        }
+
+        return jwtService.generateToken(username);
     }
 }
