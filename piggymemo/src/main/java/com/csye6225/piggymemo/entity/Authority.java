9bcdfd254aad20889bd0d5979c6d6a4bc83bbb24
Yuntian <xiuqizhiping123@gmail.com>
2026-07-24 00:58:41 +0800

    Add Authority entity and repository

---
+++ piggymemo/src/main/java/com/csye6225/piggymemo/entity/Authority.java
@@ -0,0 +1,48 @@
+package com.csye6225.piggymemo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "authorities")
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    /**
+     * 用户角色
+     */
+    @Column(name = "identity", length = Integer.MAX_VALUE)
+    private String identity;
+
+    @NotNull
+    @Column(name = "uid", nullable = false)
+    private Long uid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+}
