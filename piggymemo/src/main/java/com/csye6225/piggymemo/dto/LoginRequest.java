81fc9d0ada91f68401421776a8f9f61541b95ee5
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-23 08:05:33 -0400

    Added login dto

---
+++ piggymemo/src/main/java/com/csye6225/piggymemo/dto/LoginRequest.java
@@ -0,0 +1,29 @@
+package com.csye6225.piggymemo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequest {
+    @NotBlank(message = "Username cannot be empty.")
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
