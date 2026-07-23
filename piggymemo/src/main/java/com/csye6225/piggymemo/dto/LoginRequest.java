8b5cade0c33f5f6746791ddc4ea8eaa1fc5dccb4
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-23 08:13:33 -0400

    Added dto and repo query

--- piggymemo/src/main/java/com/csye6225/piggymemo/dto/LoginRequest.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/dto/LoginRequest.java
@@ -1,7 +1,6 @@
 package com.csye6225.piggymemo.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public class LoginRequest {
     @NotBlank(message = "Username cannot be empty.")
--- piggymemo/src/main/java/com/csye6225/piggymemo/repository/UserRepository.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.csye6225.piggymemo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.csye6225.piggymemo.entity.User;
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public boolean existsByUsername(String username);
+    public User findByUsername(String username);
 }
