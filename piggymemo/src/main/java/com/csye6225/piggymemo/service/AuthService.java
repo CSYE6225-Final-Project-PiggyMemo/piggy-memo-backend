afa9642c83ce6466ad22910e3a610db2d69a840a
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-23 01:29:10 -0400

    Initialized auth service

--- piggymemo/src/main/java/com/csye6225/piggymemo/service/AuthService.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/service/AuthService.java
@@ -1,5 +1,17 @@
 package com.csye6225.piggymemo.service;
 
-public class AuthService {
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.csye6225.piggymemo.repository.UserRepository;
+
+public class AuthService {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(JwtService jwtService, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
