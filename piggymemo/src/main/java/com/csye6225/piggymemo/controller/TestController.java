9cfea534d50466a5afdae8e0b7e1e617f0e46f42
Yuntian <xiuqizhiping123@gmail.com>
2026-07-28 10:25:27 +0800

    update TestController

--- piggymemo/src/main/java/com/csye6225/piggymemo/controller/TestController.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/controller/TestController.java
@@ -1,20 +1,19 @@
 package com.csye6225.piggymemo.controller;
 
-import java.util.Map;
-
-import org.springframework.security.core.Authentication;
+import com.csye6225.piggymemo.entity.CurrentUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/me")
 public class TestController {
     @GetMapping()
-    public Map<String, String> getMethodName(Authentication auth) {
-        String name = auth.getName();
+    public Map<String, String> getMethodName(@AuthenticationPrincipal CurrentUser currentUser) {
+        String name = currentUser.username();
         return Map.of("username", name);
     }
 }
