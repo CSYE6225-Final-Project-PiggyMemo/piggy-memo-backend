2978a6777c89e37f2da5f9b741ba1932b2364b74
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-28 22:19:14 -0400

    Fixed the bug that frontend jumped back to login page: enabled cors preflight requests

--- piggymemo/src/main/java/com/csye6225/piggymemo/config/SecurityConfig.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.csye6225.piggymemo.filter.JwtAuthenticationFilter;
@@ -34,6 +35,7 @@ public class SecurityConfig {
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests(
                 auth -> auth
+                    .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/users/*").permitAll()
                     .requestMatchers("/api/auth/login").permitAll()
