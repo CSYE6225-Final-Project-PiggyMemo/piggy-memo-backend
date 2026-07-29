679ffbe3fbca50fa26659e347097d8005cfeb734
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-28 20:39:26 -0400

    Added expiration time and jti to jwt payload

--- piggymemo/src/main/java/com/csye6225/piggymemo/entity/JwtPayload.java
+++ piggymemo/src/main/java/com/csye6225/piggymemo/entity/JwtPayload.java
@@ -1,6 +1,7 @@
 package com.csye6225.piggymemo.entity;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
-public record JwtPayload(Long userId, String username, List<String> authorities) {
+public record JwtPayload(Long userId, String username, List<String> authorities, String jti, OffsetDateTime expiresAt) {
 }
