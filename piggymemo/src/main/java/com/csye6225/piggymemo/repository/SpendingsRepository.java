2b48b8227de6e09ddda9353ee2c9433eb4532665
Caboose-Chengkun-Liao <jamessmithhunk@163.com>
2026-07-31 14:28:58 -0400

    Added repo layer for spendings

---
+++ piggymemo/src/main/java/com/csye6225/piggymemo/repository/SpendingsRepository.java
@@ -0,0 +1,9 @@
+package com.csye6225.piggymemo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.csye6225.piggymemo.entity.Spendings;
+
+public interface SpendingsRepository extends JpaRepository<Spendings, Long> {
+    
+}
